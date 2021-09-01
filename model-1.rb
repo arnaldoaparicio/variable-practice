0fcc7ba8598a3397598d304d5b8fa799c4fe7689
Arnaldo Aparicio <apaarn2211@gmail.com>
2021-08-31 20:24:43 -0400

    Add hash variables

--- model-1.rb
+++ model-1.rb
@@ -11,3 +11,10 @@ is_used = false
 
 student_names = ["Aaron", "James", "Sarah"]
 id_numbers = [1, 2, 3, 4]
+
+
+hash_sample = {"fruit" => "watermelon", "veggie" => "carrot", 13 => "secrets"}
+
+student = Hash.new
+student[:name] = "Cody"
+puts student[:name]
